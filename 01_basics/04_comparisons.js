f492ddd7f6428edcae06603752662b6934184ac4
Pratyush Panda <163556479+pratyushPanda23@users.noreply.github.com>
2026-01-02 18:41:47 +0000

    Update 04_comparisons.js

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -1,3 +1,13 @@
-console.log("2">1);
-console.log("02">1);
+// console.log("2">1);
+// console.log("02">1);
+
+
+console.log(null > 0);
+console.log(null == 0);//equality check and comparison works differently
+console.log(null >= 0);//in comparison null is turned to 0 that's why >= is correct over here!!
+//for undefined all the results are false 
+
+
+// === //it checks the value as well as the datatyoe 
+
 
