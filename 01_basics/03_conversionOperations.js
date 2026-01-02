7ae96e5360080bf16e31eb7089f5d75eb179d7ec
Pratyush Panda <163556479+pratyushPanda23@users.noreply.github.com>
2026-01-02 15:20:53 +0000

    Update 01_basics/03_conversionOperations.js

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);//it will give number
+// console.log(valueInNumber); 
+
+/*it gives NaN..lol(when score is a string )
+
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0;
+*/
+
+let isLoggedIn = 1;
+
+let boolIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolIsLoggedIn);
+
+
