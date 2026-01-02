8d1cf0c353af1494ddb8f5a3565129f467bcae47
Pratyush Panda <163556479+pratyushPanda23@users.noreply.github.com>
2026-01-02 19:01:07 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,18 @@
+// PRIMITIVE
+// 7 types: String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+
+// NON-PRIMITIVE
+//Reference => Array,Objects,Functions
+
+const heros = ["shaktiman","doga"]
+let myObj={
+    name:"Pratyush",
+    age:21
+}
+
+const myFunction= function(){
+    console.log("Hello World!");
+    
+}
