facabececa81e5ff1ad7b705284b3018b267bc9d
Pratyush Panda <163556479+pratyushPanda23@users.noreply.github.com>
2026-01-22 16:30:28 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,15 @@
+//singleton
+
+//object literals
+// object.create
+const JS_user = {
+  name: "Pratyush",
+  age: 21,
+  location: "Jaipur",
+  email: "pratyushpanda881@gmail.com",
+  isLoggedIn: false
+};
+
+console.log(JS_user["email"]);
+console.log("hello");
+
